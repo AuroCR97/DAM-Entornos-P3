9a54971cd37099709f5a1908d70594330a5a3cae
Saphi <Saphi@DESKTOP-7D79RVN>
2023-03-20 19:51:09 +0100

    Factorial realizado

---
+++ src/main/java/ieseuropa/NumeroPart2.java
@@ -0,0 +1,23 @@
+package main.java.ieseuropa;
+
+import java.util.Scanner;
+
+public class NumeroPart2 {
+	
+	private static int calcularFactorial(int num) {
+		int resultado = 1, i=2;
+		while(i <= num) {
+			resultado = resultado *i;
+			i++;
+		}return resultado;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("El factorial es:" + calcularFactorial(8));
+		
+
+	}
+
+}
