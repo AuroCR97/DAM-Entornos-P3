d48f68660f978fd76acfff63ac277d96d818b634
Rocio <Rocio@DESKTOP-7D79RVN>
2023-03-23 13:19:21 +0100

    Añadido metodo  repetir texto.

--- src/main/java/ieseuropa/Cadena.java
+++ src/main/java/ieseuropa/Cadena.java
@@ -12,12 +12,25 @@ public class Cadena {
 		}
 		return contador;
 	}
+	private static void repetirTexto(String texto1,String texto2,int repetir) {
+			
+			for(int i=0;i< repetir;i++) {
+				System.out.println(texto1);
+				System.out.println(texto2);
+			}
+			
+		}
 
 	public static void main(String[] args) {
 
 		String cadena = "hola mundo";
 		char letra = 'a';
 		System.out.println("La letra aparece " + contarLetra(cadena,letra) + " veces");
+		
+		String texto1 = "hola";
+		String texto2 = "mundo";
+		int repetir = 5 ;
+		repetirTexto(texto1,texto2,repetir);
 		
 
 	}
