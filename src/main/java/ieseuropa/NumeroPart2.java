bf1be6df9b2121116036f2234625ce726be11e5e
Rocio <Saphi@DESKTOP-7D79RVN>
2023-03-23 12:58:57 +0100

    Añadido metodo calcula factorial.

--- src/main/java/ieseuropa/NumeroPart2.java
+++ src/main/java/ieseuropa/NumeroPart2.java
@@ -15,33 +15,11 @@ public class NumeroPart2 {
 
 	public static void main(String[] args) {
 		
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("Introduce un numero entero para saber su factorial.");
-		int	num = teclado.nextInt();
+		int	num = 8;
 		System.out.println("El factorial es: " + calcularFactorial(num));
-		System.out.println("Dime un numero entero para saber cuantas horas,minutos y segundos son:");
 		
-		int	segundos = teclado.nextInt();
-		int hora = segundos /3600;
-		segundos %= 3600;
-		int minutos = segundos / 60 ;
-		segundos %= 60 ;
-		System.out.println( "Son "+hora + " horas - " + minutos + " minutos - "+ segundos + " segundos");
 		
-		System.out.println("Los 15 números de la serie de Pell :");
-			int pellPrevio = 0; // P(0)
-	        int pellActual = 1; // P(1)
-
-	        System.out.println(pellPrevio);
-	        System.out.println(pellActual);
-	        
-	        for (int i = 2; i < 15; i++) {
-	            int pellSiguiente = 2 * pellActual + pellPrevio;
-	            System.out.println(pellSiguiente);
-	            pellPrevio = pellActual;
-	            pellActual = pellSiguiente;
-	        }
+			
 	    }
-	
 }
 
