1f10be048ecc331be0e2c6cd4aee7ffd49cc1156
NeilyGuerra <niyeing07@gmail.com>
2023-03-26 00:49:07 +0100

    envio de array maxi y mini

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,14 +1,32 @@
 package main.java.ieseuropa;
-
+import java.util.*;
 public class Array {
 
-	private static void nada() {
-		//No hago nada
-	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
-	}
+		Scanner sc = new Scanner (System.in);
+		
+		int []num = new int [10];
+		int i;
+		System.out.println("Introduce los elementos del array");
+		for (i=0; i<10; i++) {
+			num[i]=sc.nextInt();
+		}
+		for (i=0; i<10; i++) {
+			System.out.print(num[i]+" ");
+		}
+		int max = num[0];
+		int min = num[0];
 
+		for (i=0; i<10; i++) {
+			if (num[i]<min) {
+				min = num[i];
+			}else if (num[i]>max) {
+				max= num[i];
+			}
+		}
+		System.out.println();
+		System.out.println("El numero mayor en el array es " + max);
+		System.out.println ("el numero menor en el array es "+ min);
+	}
 }
