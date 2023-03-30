e3f5fbba675c90420898210e9089609ae96d75f9
AuroCR97 <auroconderello@gmail.com>
2023-03-30 16:58:42 +0200

    Dev Revisada

--- src/main/java/ieseuropa/Cadena.java
+++ src/main/java/ieseuropa/Cadena.java
@@ -2,88 +2,64 @@ package main.java.ieseuropa;
 
 public class Cadena {
 
-	
-
-	public static int longitud(String string) {
-
+	private static int longitud(String string) {
 		int size = string.length();
-
 		return size;
-
 	}
 
-	
-	public static String mayus(String string) {
-
+	private static String mayus(String string) {
 		return string = string.toUpperCase();
-
 	}
-	//--
-	private static int contarLetra(String cadena,char letra) {
+
+	private static int contarLetra(String cadena, char letra) {
 		int contador = 0;
-		
-		for(int i=0;i<cadena.length();i++) {
-			if(cadena.charAt(i) ==  letra) {
+		for (int i = 0; i < cadena.length(); i++) {
+			if (cadena.charAt(i) == letra) {
 				contador++;
 			}
 		}
 		return contador;
 	}
 
-	private static void repetirTexto(String texto1,String texto2,int repetir) {
-			
-			for(int i=0;i< repetir;i++) {
-				System.out.println(texto1);
-				System.out.println(texto2);
-			}
+	private static void repetirTexto(String texto1, String texto2, int repetir) {
+		for (int i = 0; i < repetir; i++) {
+			System.out.println(texto1);
+			System.out.println(texto2);
 		}
-	
+	}
+
 	private static String ocurrencia(String texto) {
 		String resultado = "";
 		for (int i = 0; i < texto.length(); i++) {
-            char letra = texto.charAt(i);
-            resultado += letra;
-            resultado += letra;
-        }
-        return resultado;
+			char letra = texto.charAt(i);
+			resultado += letra;
+			resultado += letra;
+		}
+		return resultado;
 	}
-//---------
-	public static String minus(String string) {
 
+	private static String minus(String string) {
 		return string = string.toLowerCase();
-
 	}
-	
-	public static void borrarOcurrencia(String a, String b) {
+
+	private static void borrarOcurrencia(String a, String b) {
 		System.out.println(a.toLowerCase().replace(b.toLowerCase(), ""));
-	  }
+	}
 
 	public static void main(String[] args) {
 
-		
-		
 		System.out.println(longitud("holaHola"));
-		
 		System.out.println(mayus("hola"));
-		
 		System.out.println(minus("HOLAS"));
-		
-		borrarOcurrencia("RealMAdrid" , "real");
-		
-		
+		borrarOcurrencia("RealMAdrid", "real");
 		String cadena = "Hola mundo";
 		char letra = 'a';
-		System.out.println("La letra aparece " + contarLetra(cadena,letra) + " veces");
-		
-		
+		System.out.println("La letra aparece " + contarLetra(cadena, letra) + " veces");
 		String texto1 = "Hola";
 		String texto2 = "mundo";
 		int repetir = 5;
-		repetirTexto(texto1,texto2,repetir);
-		
+		repetirTexto(texto1, texto2, repetir);
 		String texto = "Hola mundo";
 		System.out.println(ocurrencia(texto));
-		
 	}
-
 }
--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -10,32 +10,67 @@ public class Numero {
 		}
 		return true;
 	}
-	
+
 	public static boolean esNumeroArmstrong(int numero) {
-	    int suma = 0;
-	    int numeroOriginal = numero;
-	    while (numero != 0) {
-	        int digito = numero % 10;
-	        suma += Math.pow(digito, 3);
-	        numero /= 10;
-	    }
-	    if (suma == numeroOriginal) {
-	    	return true;
-	    }else {
-	    	return false;
-	    }
+		int suma = 0;
+		int numeroOriginal = numero;
+		while (numero != 0) {
+			int digito = numero % 10;
+			suma += Math.pow(digito, 3);
+			numero /= 10;
+		}
+		if (suma == numeroOriginal) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	 public static boolean esDeLaSuerte(int n){
-	    	int counter=2;
-	        	
-			if (counter > n)
-			    return true;
-			if (n % counter == 0)
-			    return false;
-			int next_position = n - (n / counter);
-			counter++;
-			return esDeLaSuerte(next_position);
-	        }
+
+	public static boolean esDeLaSuerte(int n) {
+		int counter = 2;
+
+		if (counter > n)
+			return true;
+		if (n % counter == 0)
+			return false;
+		int next_position = n - (n / counter);
+		counter++;
+		return esDeLaSuerte(next_position);
+	}
+
+	private static int calcularFactorial(int num) {
+		int resultado = 1, i = 2;
+		while (i <= num) {
+			resultado = resultado * i;
+			i++;
+		}
+		return resultado;
+	}
+
+	private static void calcularHora(int segundos) {
+		int hora = segundos / 3600;
+		segundos %= 3600;
+		System.out.print("Son " + hora + " horas, ");
+		int minutos = segundos / 60;
+		System.out.print(minutos + " minutos, ");
+		segundos %= 60;
+		System.out.println(segundos + " segundos.");
+	}
+
+	private static void seriePell() {
+		int pellPrevio = 0;
+		int pellActual = 1;
+
+		System.out.println(pellPrevio);
+		System.out.println(pellActual);
+
+		for (int i = 2; i < 15; i++) {
+			int pellSiguiente = 2 * pellActual + pellPrevio;
+			System.out.println(pellSiguiente);
+			pellPrevio = pellActual;
+			pellActual = pellSiguiente;
+		}
+	}
 
 	public static void main(String[] args) {
 
@@ -44,7 +79,14 @@ public class Numero {
 		System.out.println(esPrimo(73));
 		System.out.println(esNumeroArmstrong(128));
 		System.out.println(esDeLaSuerte(35));
-		
+		int num = 8;
+		System.out.println("El factorial es: " + calcularFactorial(num));
+
+		int segundos = 25542;
+		calcularHora(segundos);
+
+		System.out.println("Los primeros 15 numeros de la serie de Pell :");
+		seriePell();
 
 	}
 
